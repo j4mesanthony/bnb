a583f8f4ab27d231b3c2303065654624ee777628
James Anthony <janthony@greenleaf.com.au>
2025-05-31 16:25:28 +1000

    feat: Added Core project, installed EF Core Sql Server and setup base context with refs to entities.

---
+++ Bnb.Core/BnbContext.cs
@@ -0,0 +1,18 @@
+﻿using Bnb.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bnb.Core;
+
+public class BnbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // TODO: Implement configuration
+    }
+
+    DbSet<User> Users { get; set; }
+    DbSet<Booking> Bookings { get; set; }
+    DbSet<Room> Rooms { get; set; }
+    DbSet<Pet> Pets { get; set; }
+    
+}
