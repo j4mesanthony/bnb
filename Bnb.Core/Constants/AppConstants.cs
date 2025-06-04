c1adf5ecb92ac8358834eb6cd8c32d2e55a910b1
James Anthony <janthony@greenleaf.com.au>
2025-06-04 21:09:06 +1000

    chore: Removed unused SettingsFile constant

--- Bnb.Core/Constants/AppConstants.cs
+++ Bnb.Core/Constants/AppConstants.cs
@@ -2,6 +2,5 @@ namespace Bnb.Core;
 
 public static class AppConstants
 {
-    public const string SettingsFile = "appsettings.json";
     public const string LocalHost = "http://localhost:5173";
 }
