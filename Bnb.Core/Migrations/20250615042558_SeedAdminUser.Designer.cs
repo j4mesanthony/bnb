bad31452337e21521b523e9bce865ed014cda7f9
James Anthony <janthony@greenleaf.com.au>
2025-06-15 14:40:58 +1000

    feat: Seeded admin user

--- Bnb.Core/Migrations/20250615042558_SeedAdminUser.Designer.cs
+++ Bnb.Core/Migrations/20250615042558_SeedAdminUser.Designer.cs
@@ -4,6 +4,7 @@ using Bnb.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bnb.Core.Migrations
 {
     [DbContext(typeof(BnbContext))]
-    partial class BnbContextModelSnapshot : ModelSnapshot
+    [Migration("20250615042558_SeedAdminUser")]
+    partial class SeedAdminUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
