f53676a3a366fba45b2521e945ba24674472afbd
James Anthony <janthony@greenleaf.com.au>
2025-06-15 14:52:16 +1000

    feat: User placeholder view

--- Bnb.App/src/App.tsx
+++ Bnb.App/src/App.tsx
@@ -1,19 +1,13 @@
-import { useFetch } from "./hooks/useFetch";
-import type { UserModel } from "./users/models/UserModel";
-import UserList from "./users/UserList";
+import Users from "./users/Users";
 
 function App() {
-  const apiBase = `${import.meta.env.VITE_API_BASE}/api`;
-  const endpoint = `${apiBase}/user`;
-  const { data } = useFetch<UserModel[]>(endpoint);
-
   return (
     <>
       <h1 className="text-8xl font-bold text-red-700 underline w-full text-center pt-28 pb-3">
         Hello.
       </h1>
 
-      {data?.length && <UserList users={data} />}
+      <Users />
     </>
   );
 }
---
+++ Bnb.App/src/users/Users.tsx
@@ -0,0 +1,11 @@
+import { useFetch } from "../hooks/useFetch";
+import type { UserModel } from "./models/UserModel";
+import UserList from "./UserList";
+
+export default function Users() {
+  const apiBase = `${import.meta.env.VITE_API_BASE}/api`;
+  const endpoint = `${apiBase}/user`;
+  const { data } = useFetch<UserModel[]>(endpoint);
+
+  return <>{data?.length && <UserList users={data} />}</>;
+}
