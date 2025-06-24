78164813b38e10c12c49bd5dbed82e471554c60d
James Anthony <janthony@greenleaf.com.au>
2025-06-24 10:14:03 +1000

    chore: Vitest coverage confiration updates.

--- Bnb.App/vitest.config.ts
+++ Bnb.App/vitest.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
+    coverage: {
+      include: ["src/**"],
+      exclude: ["src/App.tsx", "src/main.tsx", "src/vite-env.d.ts", "src/features/*/*.tsx"],
+    },
   },
 });
