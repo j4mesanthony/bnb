9f991d544e6ad99c5af33e57659002add00865d8
James Anthony <janthony@greenleaf.com.au>
2025-06-30 08:39:57 +1000

    test: PrimaryButton test for click handler.

--- Bnb.App/src/components/__tests__/PrimaryButton.test.tsx
+++ Bnb.App/src/components/__tests__/PrimaryButton.test.tsx
@@ -1,21 +1,27 @@
-import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
 import PrimaryButton from "../PrimaryButton";
 
 describe("PrimaryButton", () => {
   it("should render with default slot content", () => {
-    render(<PrimaryButton />);
+    render(<PrimaryButton handleClick={() => {}} />);
     const element = screen.getByRole("button", { name: /PrimaryButton/i });
     expect(element).toBeDefined();
   });
+
   it("should render with provided slot content", () => {
-    render(<PrimaryButton>Save</PrimaryButton>);
+    render(<PrimaryButton handleClick={() => {}}>Save</PrimaryButton>);
     const element = screen.getByRole("button", { name: /save/i });
     expect(element).toBeDefined();
   });
-  // it("should emit a click event", () => {
-  //   render(<PrimaryButton>Click Me</PrimaryButton>);
-  //   const element = screen.getByRole("button", { name: /click me/i });
-  //   expect(element).toBeDefined();
-  // });
+
+  it("should emit a click event", () => {
+    const mockFn = vi.fn();
+    render(<PrimaryButton handleClick={mockFn}>Click Me</PrimaryButton>);
+    const element = screen.getByRole("button", { name: /click me/i });
+
+    fireEvent.click(element);
+    expect(element).toBeDefined();
+    expect(mockFn).toHaveBeenCalledOnce();
+  });
 });
