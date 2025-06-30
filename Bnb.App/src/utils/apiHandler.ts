9753d97c085ae4683fe39b2ca9fbcb9dcc05b9fe
James Anthony <janthony@greenleaf.com.au>
2025-06-30 10:36:20 +1000

    feat: Basic apiHandler util function with get.

---
+++ Bnb.App/src/utils/apiHandler.ts
@@ -0,0 +1,34 @@
+type ApiHandler = {
+  errorHandler: (error: Error) => void;
+  get: (url: string, options?: RequestInit) => Promise<void>;
+  responseHandler: (reponse: Response) => void;
+};
+
+export const apiHandler: ApiHandler = {
+  get: function (
+    url: string,
+    options = { method: "GET", headers: { Accept: "application/json" } }
+  ) {
+    return fetch(url, options)
+      .then(this.responseHandler)
+      .catch(this.errorHandler);
+  },
+
+  responseHandler: function (response: Response) {
+    const { ok, status } = response;
+
+    if (!ok) {
+      throw new Error(`HTTP Error! ${status}`);
+    } else {
+      return response;
+    }
+  },
+
+  errorHandler: function (error: unknown) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error(error);
+    }
+  },
+};
