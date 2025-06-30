e14f9badd0d810adce5dac27c05a14d41b9a0bc9
James Anthony <janthony@greenleaf.com.au>
2025-06-30 20:27:02 +1000

    feat: Post method in apiHandler.

--- Bnb.App/src/utils/apiHandler.ts
+++ Bnb.App/src/utils/apiHandler.ts
@@ -1,5 +1,6 @@
 type ApiHandler = {
   get: <T>(url: string, options?: RequestInit) => Promise<T>;
+  post: <T>(url: string, dto: T, options?: RequestInit) => Promise<T>;
   responseHandler: <T>(response: Response) => Promise<T>;
 };
 
@@ -10,6 +11,23 @@ export const apiHandler: ApiHandler = {
       headers: { Accept: "application/json", ...(options.headers || {}) },
       ...options,
     };
+    return fetch(url, opts).then((response) =>
+      apiHandler.responseHandler<T>(response)
+    );
+  },
+
+  post: <T>(url: string, dto: T, options: RequestInit = {}) => {
+    const opts = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        ...(options.headers || {}),
+      },
+      body: JSON.stringify(dto),
+      ...options,
+    };
+
     return fetch(url, opts).then((response) =>
       apiHandler.responseHandler<T>(response)
     );
