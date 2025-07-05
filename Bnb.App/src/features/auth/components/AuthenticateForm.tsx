f33dbf5690d9eacf7c1be78458e33d281211a54b
James Anthony <janthony@greenleaf.com.au>
2025-07-05 19:05:38 +1000

    feat: Implemented state reducer in AuthForm.

--- Bnb.App/src/features/auth/components/AuthenticateForm.tsx
+++ Bnb.App/src/features/auth/components/AuthenticateForm.tsx
@@ -1,13 +1,40 @@
+import { useReducer } from "react";
 import DataInput from "../../../components/DataInput";
 import PrimaryButton from "../../../components/PrimaryButton";
+import type { UserCredentials } from "../../dtos/userCredentials";
 
 type AuthenticateFormProps = {
-  handleSubmit: () => void;
+  handleSubmit: (dto: UserCredentials) => void;
+};
+
+type State = {
+  email: string;
+  password: string;
+};
+
+type StateAction =
+  | { type: "EMAIL"; payload: string | number }
+  | { type: "PASSWORD"; payload: string | number };
+
+const credentialsReducer = (state: State, action: StateAction): State => {
+  switch (action.type) {
+    case "EMAIL":
+      return { email: action.payload as string, password: state.password };
+    case "PASSWORD":
+      return { email: state.email, password: action.payload as string };
+    default:
+      return state;
+  }
 };
 
 export default function AuthenticateForm({
   handleSubmit,
 }: AuthenticateFormProps) {
+  const [state, dispatch] = useReducer(credentialsReducer, {
+    email: "",
+    password: "",
+  });
+
   return (
     <>
       <div className="mb-5">
@@ -15,7 +42,8 @@ export default function AuthenticateForm({
           id="email"
           label="Email"
           type="email"
-          handleChange={() => {}}
+          value={state.email}
+          handleChange={(payload) => dispatch({ type: "EMAIL", payload })}
         />
       </div>
       <div className="mb-5">
@@ -23,7 +51,8 @@ export default function AuthenticateForm({
           id="password"
           label="Password"
           type="password"
-          handleChange={() => {}}
+          value={state.password}
+          handleChange={(payload) => dispatch({ type: "PASSWORD", payload })}
         />
       </div>
       <div className="mb-5">
@@ -31,11 +60,21 @@ export default function AuthenticateForm({
           id="confirm"
           label="Confirm Password"
           type="password"
+          value=""
           handleChange={() => {}}
         />
       </div>
 
-      <PrimaryButton handleClick={handleSubmit}>Login</PrimaryButton>
+      <PrimaryButton
+        handleClick={() =>
+          handleSubmit({
+            email: state.email,
+            password: state.password,
+          })
+        }
+      >
+        Login
+      </PrimaryButton>
     </>
   );
 }
