a7f1cc42768456042dfd1efbb35264c55a8034cd
James Anthony <janthony@greenleaf.com.au>
2025-07-14 16:31:46 +1000

    feat: Set loading status on login hit.

--- Bnb.App/src/features/auth/AuthenticationView.tsx
+++ Bnb.App/src/features/auth/AuthenticationView.tsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
 import { authenticationApi } from "./api/authenticationApi";
 import AuthenticateForm from "./components/AuthenticationForm";
 import type { UserCredentialsDto } from "./dtos/UserCredentialsDto";
 
 export default function AuthenticationView() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const handleLogin = async (dto: UserCredentialsDto) => {
-    await authenticationApi.login(dto);
+    setIsLoading(true);
+
+    try {
+      await authenticationApi.login(dto);
+    } catch (error) {
+      console.error(error);
+    }
+
+    setIsLoading(false);
   };
 
   return (
     <>
       <div className="w-full max-w-md">
-        <AuthenticateForm handleSubmit={handleLogin} />
+        <AuthenticateForm handleSubmit={handleLogin} isLoading={isLoading} />
       </div>
     </>
   );
